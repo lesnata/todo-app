e620d29ca4abbcc249e51c44e6463e963df7a75b
lesnata <lesyk.natalie@gmail.com>
2021-01-16 22:47:43 +0200

    working on registration form

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+# from ./api/views.py import UserRegistration
 
 
 urlpatterns = [
     path('', views.list, name='list'),
+    path('registration/', views.registration, name='registration'),
+    # path('/auth/users/', views.UserRegistration.as_view(), name="authorization")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from djoser.serializers import UserCreateSerializer
 # Create your views here.
 
 
 def list(request):
-
     return render(request, 'frontend/list.html')
+
+
+def registration(request):
+    return render(request, 'frontend/registration.html')
