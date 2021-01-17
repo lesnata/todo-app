032d1de127e73c4128d4b6d2facfb662953e3127
lesnata <lesyk.natalie@gmail.com>
2021-01-17 18:50:34 +0200

    registration form

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from djoser.serializers import UserCreateSerializer
+#from djoser.serializers import UserCreateSerializer
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -8,4 +9,13 @@ def list(request):
 
 
 def registration(request):
-    return render(request, 'frontend/registration.html')
+    form = UserCreationForm()
+    context = {'form': form}
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    return render(request, 'frontend/registration.html', context)
